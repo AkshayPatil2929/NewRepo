fcca91c9f0e62fb23e42b4ed634043a4bd31bb29
AkshayPatil2929 <120776802+AkshayPatil2929@users.noreply.github.com>
2022-12-17 14:29:42 +0530

    Add files via upload

---
+++ Day11/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly asm = Assembly.LoadFile(@"G:\Dot Net\By SIr\Day11\ReflectionExample\UKCalculator\bin\Debug\UKCalculator.dll");
+           
+            Type t = asm.GetType(asm.GetName().Name + ".TaxCalculator", true, true);
+            Console.WriteLine("type is : " +t.GetTypeInfo());
+            object obj = Activator.CreateInstance(t);
+            Console.WriteLine("Object type  is : " + obj.GetType());
+    
+            
+            PropertyInfo pi=t.GetProperty("Basic");
+            pi.SetValue(obj, 45000.0M, null);
+            Console.WriteLine(pi.GetValue(obj,null).ToString());
+           
+
+
+           MethodInfo mi= t.GetMethod("GetNetSalary");
+            decimal NetSal =((decimal)mi.Invoke(obj,null));
+            Console.WriteLine(NetSal.ToString());
+            Console.ReadLine();
+        }
+
+        static void Main1(string[] args)
+        {
+            Assembly asmb = Assembly.LoadFile(@"G:\Dot Net\By SIr\Day11\ReflectionExample\UKCalculator\bin\Debug\UKCalculator.dll");
+
+           Type t1 = asmb.GetType(asmb.GetName().Name + ".TaxCalculator", true, true);
+            Object obj = Activator.CreateInstance(t1);
+
+            PropertyInfo pi1=t1.GetProperty("Basic");
+            pi1.SetValue(obj, 45000.00M, null);
+            Console.WriteLine(pi1.GetValue(obj, null).ToString());
+
+            MethodInfo mi1 = t1.GetMethod("GetNetSalary");
+          decimal sal=  ((decimal)mi1.Invoke(obj, null));
+            Console.WriteLine(sal);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day11/ConsoleApp1/FileOperations/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Directory.CreateDirectory(@"G:\Dot Net\FileCreated");
+         //   File.Create(@"G:\Dot Net\FileCreated\f.txt");
+            File.Create(@"G:\Dot Net\FileCreated\f.txt");
+            string st = "Akshay";
+            byte[] arr = Encoding.Default.GetBytes(st);
+            stream.
+                //.Write(arr, 0, arr.Length);
+
+        }
+    }
+}
